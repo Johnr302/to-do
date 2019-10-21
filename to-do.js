0c2b5c5744e67426ec5caa1c27e258d516719d52
Johnr302 <johnathon.regalado@gmail.com>
2019-10-20 21:52:11 -0700

    adding JavaScript and changed to-do.html to index.html

--- to-do.js
+++ to-do.js
@@ -1,9 +1,25 @@
 const listsContainer = document.queryCommandEnabled("[data-list]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
+const deleteListButton = document.querySelector("data-delete-list-button");
 
 const LOCAL_STORAGE_LIST_KEY = "tasks.list";
+let selectedList = (LOCAL_SELECTED_LIST_ID_KEY = "task.selectedListId");
 let list = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_SELECTED_LIST_ID_KEY);
+
+listsContainer.addEventListener("click", e => {
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
+
+deleteListButton.addEventListener("click", e => {
+  lists = list.filter(list => list.id !== selectedListId);
+  selectedListId = null;
+  saveAndRender();
+});
 
 newListForm.addEventListener("submit", e => {
   e.preventDefault();
@@ -23,7 +39,15 @@ let createList = name => {
   };
 };
 
-// START here at 9:23 https://www.youtube.com/watch?v=W7FaYfuwu70
+let saveAndRender = () => {
+  save();
+  render();
+};
+
+let save = () => {
+  localStorage.setItem(JSON.stringify(lists));
+  localStorage.setItem(LOCAL_SELECTED_LIST_ID_KEY, selectedListId);
+};
 
 let render = () => {
   clearElement(listsContainer);
@@ -32,6 +56,9 @@ let render = () => {
     listElement.dataset.listId = list.id;
     listElement.classList.add("list-name");
     listElement.innerText = list;
+    if (list.id === selectedListId) {
+      listElement.classList.add("active-list");
+    }
     listsContainer.appendChild(listElement);
   });
 };
